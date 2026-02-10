d7f087948acb214b8082918e1189e98d155c03e5
rayhan108 <raihanshorker56@.com>
2026-02-10 14:57:16 +0600

    initial project setup

---
+++ src/app.ts
@@ -0,0 +1,55 @@
+// c:\STA\El-afrik\src\app.ts (FIXED)
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+
+import globalErrorHandler from './app/middleware/globalErrorHandler';
+import notFound from './app/middleware/notFound';
+import morgan from 'morgan';
+
+import router from './app/routes';
+
+const app: Application = express();
+
+
+
+
+app.use(
+  cors({
+    origin: [
+      'http://10.10.20.13:5000',
+      'http://10.10.20.13:3000',
+      'http://localhost:5175',
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'http://localhost:3000',
+      'https://el-afrik.vercel.app',
+      'http://15.223.245.199',
+      'http://15.223.245.199:3000',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  })
+);
+
+
+
+// Now apply JSON parser for all other routes
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+
+
+app.use(morgan('dev'));
+app.use('/api/v1', router);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server is Running...');
+});
+
+app.use(globalErrorHandler);
+app.use(notFound);
+
+export default app;
---
+++ src/app/modules/User/user.services.ts
@@ -0,0 +1,87 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import AppError from "../../errors/AppError";
+import { TEditProfile } from "./user.constant";
+import httpStatus from 'http-status';
+import { UserModel } from "./user.model";
+import QueryBuilder from "../../builder/QueryBuilder";
+
+
+
+const updateProfileFromDB = async (id: string, payload: TEditProfile) => {
+  const firstName=payload.firstName
+const lastName=payload.lastName
+const fullName=`${firstName} ${lastName}`
+payload.fullName=fullName
+  const result = await UserModel.findByIdAndUpdate(id, payload, {
+    new: true,
+  });
+
+  return result;
+};
+const getMyProfileFromDB = async (id: string, ) => {
+  const result = await UserModel.findById(id);
+
+  return result;
+};
+const getSingleProfileFromDB = async (id: string, ) => {
+  const result = await UserModel.findById(id);
+
+  return result;
+};
+
+
+const deletePrifileFromDB = async (id: string) => {
+  const event = await UserModel.findByIdAndDelete(id);
+
+  if (!event) {
+    throw new AppError(httpStatus.NOT_FOUND, 'User not found!');
+  }
+
+  return event; // return deleted user if neededd
+};
+const deleteUserFromDB = async (id: string) => {
+  const event = await UserModel.findByIdAndDelete(id);
+
+  if (!event) {
+    throw new AppError(httpStatus.NOT_FOUND, 'User not found!');
+  }
+
+  return event; // return deleted user if neededd
+};
+const getAllUserFromDB = async (query: Record<string, unknown>) => {
+  const queryBuilder = new QueryBuilder(UserModel.find(), query);
+  queryBuilder.search(["firstName", "lastName", "email", "role"]).filter().sort().paginate();
+  const result = await queryBuilder.modelQuery;
+  const meta = await queryBuilder.countTotal();
+
+  return { meta, result };
+};
+const blockUserFromDB = async (id: string, status: string) => {
+
+  const user = await UserModel.findById(id);
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, 'User not found!');
+  }
+
+
+  const result = await UserModel.findByIdAndUpdate(
+    id,
+    { status },
+    { new: true, runValidators: true }
+  );
+
+  return result;
+};
+
+
+
+
+
+export const UserServices = {
+  updateProfileFromDB,
+  getMyProfileFromDB,
+  deletePrifileFromDB,
+  getAllUserFromDB,getSingleProfileFromDB,deleteUserFromDB,blockUserFromDB
+
+};
